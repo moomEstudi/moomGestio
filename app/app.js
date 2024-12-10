dd84a8a0926370c844b0ddda16e3c0586dcdc1d5
moomEstudi <antonillull@coaib.es>
2024-12-10 16:45:26 +0100

    we

--- app/app.js
+++ app/app.js
@@ -1,48 +1,33 @@
-// Inicialització de l'aplicació AngularJS
+// Inicialització de l'aplicació AngularJS amb ngRoute i AngularFire
 var app = angular.module('accountingApp', ['ngRoute', 'firebase']);
 
 // Configuració de rutes
 app.config(function($routeProvider) {
-    $routeProvider
-      .when('/login', {
-        templateUrl: 'app/views/login.html',
-        controller: 'AuthController'
-      })
-      .when('/expenses', {
-        templateUrl: 'app/views/expenses.html',
-        controller: 'ExpenseController'
-      })
-      .otherwise({
-        redirectTo: '/login'
-      });
-  });
-  
-
-// Inicialització de Firebase
-/* app.run(function() {
-    const firebaseConfig = {
-        apiKey: "AIzaSyB18AEfz9T1ncXWG7gbdv_VO8pFXuJqnDM",
-        authDomain: "moomgestio.firebaseapp.com",
-        projectId: "moomgestio",
-        storageBucket: "moomgestio.firebasestorage.app",
-        messagingSenderId: "400919933515",
-        appId: "1:400919933515:web:f35a7425821719350913ce"
-      };
-  firebase.initializeApp(firebaseConfig);
+  $routeProvider
+    .when('/login', {
+      templateUrl: 'app/views/login.html',
+      controller: 'AuthController'
+    })
+    .when('/expenses', {
+      templateUrl: 'app/views/expenses.html',
+      controller: 'ExpenseController'
+    })
+    .otherwise({
+      redirectTo: '/login'
+    });
 });
- */
+
 app.run(['$rootScope', '$location', function($rootScope, $location) {
     firebase.auth().onAuthStateChanged(function(user) {
       if (user) {
-        // Usuari autenticat
-        $rootScope.user = user;
+        $rootScope.user = user; // Desa l'usuari al $rootScope
       } else {
-        // Redirigeix al login si no està autenticat
         $rootScope.user = null;
         if ($location.path() !== '/login') {
-          $location.path('/login');
+          $location.path('/login'); // Redirigeix si no està autenticat
         }
       }
     });
   }]);
+  
   
