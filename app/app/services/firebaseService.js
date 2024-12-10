357f1192584237e1f1dcbf39c8bd68bad5658b1a
moomEstudi <antonillull@coaib.es>
2024-12-10 17:01:20 +0100

    as

--- app/app/services/firebaseService.js
+++ app/app/services/firebaseService.js
@@ -7,4 +7,11 @@ app.factory('FirebaseService', ['$firebaseArray', function($firebaseArray) {
       }
     };
   }]);
-  
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      console.log("Usuari connectat:", user.email);
+    } else {
+      console.log("No hi ha cap usuari connectat.");
+    }
+  });
+    
