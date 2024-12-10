5cbc1cb45f562bf01556ecfc180fe2f3667063aa
moomEstudi <antonillull@coaib.es>
2024-12-10 17:00:20 +0100

    ew

--- app/app.js
+++ app/app.js
@@ -1,3 +1,15 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB18AEfz9T1ncXWG7gbdv_VO8pFXuJqnDM",
+    authDomain: "moomgestio.firebaseapp.com",
+    projectId: "moomgestio",
+    storageBucket: "moomgestio.firebasestorage.app",
+    messagingSenderId: "400919933515",
+    appId: "1:400919933515:web:f35a7425821719350913ce"
+  };
+
+  // Inicialitza Firebase
+firebase.initializeApp(firebaseConfig);
+
 // Inicialització de l'aplicació AngularJS amb ngRoute i AngularFire
 var app = angular.module('accountingApp', ['ngRoute', 'firebase']);
 
